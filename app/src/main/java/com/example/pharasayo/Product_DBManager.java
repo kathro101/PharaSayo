e5f3e4973a894c01972b38c61e9babe59cf745dd
The_mailman <johndalida54@gmail.com>
2022-01-19 14:56:36 +0800

    Change variable name

--- app/src/main/java/com/example/pharasayo/Product_DBManager.java
+++ app/src/main/java/com/example/pharasayo/Product_DBManager.java
@@ -1,11 +1,13 @@
 package com.example.pharasayo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Product_DBManager {
-    static class DB_account_register extends SQLiteOpenHelper {
+    static class DB_Product extends SQLiteOpenHelper {
 
         private static  final String DB_NAME = "myDB";
         private static final String TABLE_NAME = "product_information";
@@ -32,7 +34,7 @@ public class Product_DBManager {
                 + ADDRESS_UNITFLOOR + " TEXT NOT NULL,"
                 + ADDRESS_CITY + " TEXT NOT NULL)";
 
-        public DB_account_register(Context context) {
+        public DB_Product(Context context) {
             super(context, DB_NAME, null, DATABASE_VERSION);
         }
 
@@ -46,5 +48,87 @@ public class Product_DBManager {
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
             onCreate(db);
         }
+    }
+
+    private Product_DBManager.DB_Product dbProduct;
+    private Context context;
+    private SQLiteDatabase database;
+
+    public Product_DBManager(Context c){
+        context = c;
+    }
+
+    public Product_DBManager open() throws SQLException {
+        dbProduct = new Product_DBManager.DB_Product(context);
+        database = dbProduct.getWritableDatabase();
+        return this;
+    }
+
+    public void close(){
+        dbProduct.close();
+    }
+
+    //This method will insert the information filled up in registration form
+    public void insert(
+            String productName,
+            String productPrice,
+            String decription,
+            String sellerName,
+            String cnum,
+            String add_street,
+            String add_unit,
+            String add_city){
+
+        ContentValues values = new ContentValues();
+
+        values.put(DB_Product.PRODUCT_NAME, productName);
+        values.put(DB_Product.PRODUCT_PRICE, productPrice);
+        values.put(DB_Product.DESCRIPTION, decription);
+        values.put(DB_Product.SELLER_NAME, sellerName);
+        values.put(DB_Product.CONTACT_NUMBER, cnum);
+        values.put(DB_Product.ADDRESS_STREETADDRESS, add_street);
+        values.put(DB_Product.ADDRESS_UNITFLOOR, add_unit);
+        values.put(DB_Product.ADDRESS_CITY, add_city);
+
+        database.insert(DB_Product.TABLE_NAME, null, values);
+
+    }
+
+    public int update(
+            String productName,
+            String productPrice,
+            String decription,
+            String sellerName,
+            String cnum,
+            String add_street,
+            String add_unit,
+            String add_city,
+            long _id){
+
+        ContentValues values = new ContentValues();
+
+        values.put(DB_Product.PRODUCT_NAME, productName);
+        values.put(DB_Product.PRODUCT_PRICE, productPrice);
+        values.put(DB_Product.DESCRIPTION, decription);
+        values.put(DB_Product.SELLER_NAME, sellerName);
+        values.put(DB_Product.CONTACT_NUMBER, cnum);
+        values.put(DB_Product.ADDRESS_STREETADDRESS, add_street);
+        values.put(DB_Product.ADDRESS_UNITFLOOR, add_unit);
+        values.put(DB_Product.ADDRESS_CITY, add_city);
+
+        int i = database.update(
+                DB_Product.TABLE_NAME,
+                values,
+                DB_Product.UID + " = " + _id,
+                null);
+
+        return i;
+    }
+
+    public void delete(long _id){
+        database.delete(
+                DB_Product.TABLE_NAME,
+                DB_Product.UID + " = " + _id,
+                null);
     }
 }

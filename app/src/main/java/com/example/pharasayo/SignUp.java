17988db1fceb27f3dbce1e6f213b448a9c4ad56d
The_mailman <johndalida54@gmail.com>
2022-01-19 11:21:07 +0800

    Sign up can now save registration form to sqlite database

--- app/src/main/java/com/example/pharasayo/SignUp.java
+++ app/src/main/java/com/example/pharasayo/SignUp.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -23,6 +24,8 @@ public class SignUp extends AppCompatActivity {
 
     private Button createAcc
             ,back;
+
+    private Acc_DBManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +45,56 @@ public class SignUp extends AppCompatActivity {
         back = findViewById(R.id.back);
 
         createAcc.setOnClickListener(v -> {
-            if(fullname.getText().toString().length() == 0){
-                fullname.setError("Enter your full name");
-            }
-            if(username.getText().toString().length() == 0){
-                username.setError("Enter your full name");
+            if(fullname.getText().toString().length() == 0 ||
+                    username.getText().toString().length() == 0 ||
+                    email.getText().toString().length() == 0 ||
+                    phone.getText().toString().length() == 0 ||
+                    password.getText().toString().length() == 0 ||
+                    address.getText().toString().length() == 0 ||
+                    unit.getText().toString().length() == 0 ||
+                    location.getText().toString().length() == 0
+            ){
+                if(fullname.getText().toString().length() == 0){
+                    fullname.setError("Enter your full name");
+                }
+                if(username.getText().toString().length() == 0){
+                    username.setError("Enter your username");
+                }
+                if(email.getText().toString().length() == 0){
+                    email.setError("Enter your email");
+                }
+                if(phone.getText().toString().length() == 0){
+                    phone.setError("Enter your phone");
+                }
+                if(password.getText().toString().length() == 0){
+                    password.setError("Enter your password");
+                }
+                if(address.getText().toString().length() == 0){
+                    address.setError("Enter your address");
+                }
+                if(unit.getText().toString().length() == 0){
+                    unit.setError("Enter your unit");
+                }
+                if(location.getText().toString().length() == 0){
+                    location.setError("Enter your location");
+                }
+            }else{
+                dbManager = new Acc_DBManager(this);
+                dbManager.open();
+
+                dbManager.insert(
+                        username.getText().toString(),
+                        password.getText().toString(),
+                        fullname.getText().toString(),
+                        email.getText().toString(),
+                        phone.getText().toString(),
+                        address.getText().toString(),
+                        unit.getText().toString(),
+                        location.getText().toString()
+                );
+                Toast.makeText(getApplicationContext(), "Account Created", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(getIntent());
             }
         });
 
